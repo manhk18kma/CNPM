6d3e3c398a2b47c6652d52ab58a0dcaf4f260437
Nguyen Van Manh <manhk18kma@gmail.com>
2024-08-28 17:45:12 +0700

    1

---
+++ beapp/src/main/java/kma/cnpm/beapp/domain/common/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package kma.cnpm.beapp.domain.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class UserDTO {
+    private Long userId;
+}
